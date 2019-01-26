e1c252a31ab403022c55bd41f0736979037edacd
Jeremy <jeremytrigger@gmail.com>
2019-01-25 21:21:23 -0300

    Created scripts
    Created base scripts for the project.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager Instance;
+    public bool talking = false;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+
+
+    }
+}
