b7d49ecd02fae63b1710ae59f19f6d32ca52ce19
Jeremy <jeremytrigger@gmail.com>
2019-01-26 10:34:00 -0300

    Basic Controlls
    Added basic controls, move left and right, jump and boolean for life.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5;
+    public float jumpHeight = 300;
+    public bool hasBabies = true;
+    float baseSpeed;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Start()
+    {
+        //Asigna valor a baseSpeed para controlar la velocidad de sprint
+        baseSpeed = speed;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            //Dashea al apretar shift 
+            speed = speed * 2;
+        }
+
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            //Al levantar shift derecho vuelve la velocidad a la normalidad
+            speed = baseSpeed;
+        }
+
+        //movimiento basico de derecha a izquierda
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+
+            transform.Translate(speed * Time.deltaTime, 0f, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0f, 0f);
+        }
+
+
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        //Si se hace contacto con el piso y se aprieta espacio, salta.
+        if (collision.gameObject.tag == "Floor" && Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpHeight * Time.deltaTime), ForceMode2D.Impulse);
+        }
+    }
+
 }
+
+
+
